95f67d219607ff44d048af8b9e6c59ac1a41a9b1
Amruta200501 <129044896+Amruta200501@users.noreply.github.com>
2024-06-03 13:53:08 +0530

    done

--- src/Controller/LeaveController.js
+++ src/Controller/LeaveController.js
@@ -1,44 +1,45 @@
-const Leave = require("../Models/LeaveSchema")
+const Leave = require("../Models/LeaveSchema");
 
 //Add Leave
 exports.addLeave = async (req, res) => {
-    try {
-        let addData = await Leave.create(req.body)
-        res.status(200).json(addData)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+  try {
+    let addData = await Leave.create(req.body);
+    res.status(200).json(addData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 //All Leave
 exports.allLeave = async (req, res) => {
-    try {
-        let result = await Leave.find()
-        .populate("EmpId")
-        res.status(200).json(result)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+  try {
+    let result = await Leave.find().populate("EmpId");
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 //Delete Leave
-exports.deleteLeave = async (res, req) => {
-    try {
-       let deleteData = await Leave.findByIdAndDelete({ _id: req.body.leaveid })
-        res.status(200).json(deleteData)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+exports.deleteLeave = async (req, res) => {
+  try {
+    let deleteData = await Leave.findByIdAndDelete({ _id: req.body.leaveid });
+    res.status(200).json(deleteData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 //Update Leave
-exports.updateLeave = async (res, req) => {
-    try {
-        const updateData = await Leave.findByIdAndUpdate(req.body.leaveid,
-            { LeaveStatus: req.body.LeaveStatus },
-            { new: true })
-        res.status(200).json(updateData)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+exports.updateLeave = async (req,res) => {
+  try {
+    const updateData = await Leave.findByIdAndUpdate(
+      req.body.leaveid,
+      { LeaveStatus: req.body.LeaveStatus },
+      { new: true }
+    );
+    res.status(200).json(updateData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,36 +1,33 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //User Routes
-const UserController = require("../Controller/UserController")
-router.post("/adduser", UserController.addUser)
-router.get("/alluser", UserController.allUser)
-router.delete("/deleteuser", UserController.deleteUser)
-router.put("/updateuser", UserController.updateUser)
-router.post("/finddesig", UserController.findDesig)
-router.post("/dologin",UserController.doLogin)
-
+const UserController = require("../Controller/UserController");
+router.post("/adduser", UserController.addUser);
+router.get("/alluser", UserController.allUser);
+router.delete("/deleteuser", UserController.deleteUser);
+router.put("/updateuser", UserController.updateUser);
+router.post("/finddesig", UserController.findDesig);
+router.post("/dologin", UserController.doLogin);
 
 //Project Routes
-const ProjectController = require("../Controller/ProjectController")
-router.post("/addproject", ProjectController.addProject)
-router.get("/allproject", ProjectController.allProject)
-router.delete("/deleteproject", ProjectController.deleteProject)
-router.put("/updateproject", ProjectController.updateProject)
-router.post("/userbyempid",ProjectController.getUserByEmpId)
+const ProjectController = require("../Controller/ProjectController");
+router.post("/addproject", ProjectController.addProject);
+router.get("/allproject", ProjectController.allProject);
+router.delete("/deleteproject", ProjectController.deleteProject);
+router.put("/updateproject", ProjectController.updateProject);
+router.post("/userbyempid", ProjectController.getUserByEmpId);
 
 //Leave Routes
-const LeaveController = require("../Controller/LeaveController")
-router.post("/addleave", LeaveController.addLeave)
-router.get("/allleave", LeaveController.allLeave)
-router.delete("/deleteleave", LeaveController.deleteLeave)
-router.put("/updateleave", LeaveController.updateLeave)
+const LeaveController = require("../Controller/LeaveController");
+router.post("/addleave", LeaveController.addLeave);
+router.get("/allleave", LeaveController.allLeave);
+router.delete("/deleteleave", LeaveController.deleteLeave);
+router.put("/updateleave", LeaveController.updateLeave);
 
 //Holiday Routes
-const HolidayController = require("../Controller/HolidayController")
-router.post("/addholiday", HolidayController.addHoliday)
-router.get("/allholiday", HolidayController.allHoliday)
+const HolidayController = require("../Controller/HolidayController");
+router.post("/addholiday", HolidayController.addHoliday);
+router.get("/allholiday", HolidayController.allHoliday);
 
-module.exports = router
-
-
+module.exports = router;
